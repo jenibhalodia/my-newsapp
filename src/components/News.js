81cf1a52ab750b591ae5b1ce548f344089c9d285
jenibhalodia <jenibhalodia9898@gmail.com>
2024-01-12 11:46:40 +0530

    newsapp

---
+++ src/components/News.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NewsItem from './NewsItem'
+
+export default function News() {
+  return (
+    <div>
+      this is news components
+      <NewsItem/>
+      <NewsItem/>
+      <NewsItem/>
+      <NewsItem/>
+      <NewsItem/>
+      <NewsItem/>
+    </div>
+  )
+}
